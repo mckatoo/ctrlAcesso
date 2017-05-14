0c51e2137754f8e73103c0464ce3dfc4efef2d8a
Milton Carlos Katoo <mckatoo@gmail.com>
2017-05-14 18:24:58 -0300

    LENDO CSV - FALTA CONVERTER PARA JSON

--- ctrl-acesso/src/app/secretaria/secretaria.component.ts
+++ ctrl-acesso/src/app/secretaria/secretaria.component.ts
@@ -8,9 +8,39 @@ import { FirebaseListObservable, FirebaseObjectObservable, AngularFireDatabase }
 })
 export class SecretariaComponent implements OnInit {
   lista: FirebaseListObservable<any>;
+  text: string = '';
+
   constructor(db: AngularFireDatabase) {
     this.lista = db.list('/teste');
   }
+
+  trocar(str:string, rgx, por:string=""){
+    return str.replace(rgx,por);
+  }
+
+  importCSV(e) {
+    var fileName = e.target.files[0];
+    console.log(fileName);
+    if (!fileName) {
+      return;
+    }
+    var reader = new FileReader();
+
+    reader.onload = file => {
+      // let str : string = encodeURI(encodeURIComponent(reader.result));
+      let str = encodeURI(reader.result);
+      str = this.trocar(str,/;/gi);
+      str = this.trocar(str,/%0D%0A/gi,'\n');
+      str = this.trocar(str,/%20/gi,' ');
+      str = this.trocar(str,/%22/gi,'"');
+      str = this.trocar(str,/%EF%BF%BD%EF%BF%BDo/gi,'ção');
+      this.text = str;
+      console.log(this.text);
+    }
+
+    reader.readAsText(fileName);
+  }
+
   save() {
     this.lista.push({
       "matricula": "31010004709",
@@ -21,14 +51,17 @@ export class SecretariaComponent implements OnInit {
       "aceite_contrato": "18/02/2016"
     });
   }
+  
   update(key: string, json: any) {
     this.lista.update(key, json);
   }
-  deleteItem(key: string) {    
-    this.lista.remove(key); 
-  }
-  deleteEverything() {
-    this.lista.remove();
+  
+  delete(key: string) {
+    if (key == 'undefined') {
+      this.lista.remove();
+    } else {
+      this.lista.remove(key); 
+    }
   }
 
   ngOnInit() {
